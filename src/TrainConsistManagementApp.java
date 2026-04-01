bb5643c0f128e58c19dc0b74740032bc5cb64600
Armaan1005 <patel.armaan1005@gmail.com>
2026-04-01 09:05:07 +0530

    Base application setup

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,9 @@
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==========================================");
+        System.out.println("Base Application Setup - Train Consist App");
+        System.out.println("==========================================\n");
+    }
+}
